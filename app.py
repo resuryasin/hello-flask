68b7e621d94529ffd05eb7b81e74e2b4c519c9d7
resuryasin <672016198@student.uksw.edu>
2022-12-12 22:42:29 +0700

    Change Hello to Hello World

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run()
